e91d6f057bdc92a4bbd39d5d5a07e5a93474aeb7
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-24 22:17:57 +0300

    fully working solution:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/repository/CampaignRepository.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/repository/CampaignRepository.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/repository/CampaignRepository.java
@@ -1,2 +1,32 @@
-package com.patika.kredinbizdeservice.repository;public class CampaignRepository {
+package com.patika.kredinbizdeservice.repository;
+
+import com.patika.kredinbizdeservice.factory.BankFactory;
+import com.patika.kredinbizdeservice.factory.CampaignFactory;
+import com.patika.kredinbizdeservice.model.Bank;
+import com.patika.kredinbizdeservice.model.Campaign;
+import com.patika.kredinbizdeservice.model.CampaignWithCC;
+
+import java.util.List;
+import java.util.Set;
+
+public class CampaignRepository {
+
+    private final List<Campaign> campaignList = CampaignFactory.getInstance().getCampaignList();
+    private final Set<Bank> bankList = BankFactory.getInstance().getBankList();
+
+    public List<CampaignWithCC> getCampaignsWithCreditCards() {
+        return bankList.stream()
+                .flatMap(bank -> bank.getCreditCardList().stream()
+                        .flatMap(creditCard -> bank.getCampaignList().stream()
+                                .map(campaign -> new CampaignWithCC(campaign, creditCard))))
+                .toList();
+    }
+
+
+    public List<Campaign> getAllOrderedWithoutCreditCard() {
+        return campaignList.stream()
+                .sorted((c1, c2) -> c2.getCreateDate().compareTo(c1.getCreateDate()))
+                .toList();
+    }
+
 }
