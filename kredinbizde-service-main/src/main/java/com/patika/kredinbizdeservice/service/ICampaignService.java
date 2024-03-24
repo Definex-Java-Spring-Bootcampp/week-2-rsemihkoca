9c7e46a4d8b43cb82a48e69f4cab1f863e3e9a3e
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-24 22:17:57 +0300

    fully working solution:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/service/ICampaignService.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/service/ICampaignService.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/service/ICampaignService.java
@@ -1,2 +1,13 @@
-package com.patika.kredinbizdeservice.service;public class ICampaignService {
+package com.patika.kredinbizdeservice.service;
+
+import com.patika.kredinbizdeservice.model.*;
+
+import java.util.List;
+
+public interface ICampaignService {
+
+    public List<CampaignWithCC> getAllCampaigns();
+    public List<Campaign> getAllCampaignsOrdered();
+
+
 }
