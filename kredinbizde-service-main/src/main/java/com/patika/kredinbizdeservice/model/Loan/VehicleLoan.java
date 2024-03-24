73ac82565b12637f8587c163ba742d3ae7150e14
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-24 20:07:09 +0300

    added week1 classes for init and swagger support:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/Loan/VehicleLoan.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/Loan/VehicleLoan.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/Loan/VehicleLoan.java
@@ -1,38 +1,37 @@
 package com.patika.kredinbizdeservice.model.Loan;
 
-import com.patika.kredinbizdenservice.enums.LoanType;
-import com.patika.kredinbizdenservice.enums.VehicleStatusType;
-import com.patika.kredinbizdenservice.factory.BankFactory;
-import com.patika.kredinbizdenservice.model.Bank;
+import com.patika.kredinbizdeservice.enums.LoanType;
+import com.patika.kredinbizdeservice.factory.BankFactory;
+import com.patika.kredinbizdeservice.model.Bank;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.util.Random;
 
+@Getter
 public class VehicleLoan extends Loan {
 
-    @Getter private LoanType loanType = LoanType.ARAC_KREDISI;
-    private VehicleStatusType VehicleStatusType;
+    private final LoanType loanType = LoanType.ARAC_KREDISI;
 
-    private VehicleLoan(BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
-        super(amount, installment, interestRate, bank);
+    private VehicleLoan(Integer id, BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
+        super(id, amount, installment, interestRate, bank);
     }
 
     @Override
     void calculate(BigDecimal amount, int installment) {
-        //istediği kadar ödeme yapabilir.
     }
 
-    public static VehicleLoan create(BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
-        return new VehicleLoan(amount, installment, interestRate, bank);
+    public static VehicleLoan create(Integer id, BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
+        return new VehicleLoan(id, amount, installment, interestRate, bank);
     }
 
     public static VehicleLoan createRandom() {
         Random random = new Random();
+        int id = random.nextInt(1, 1_000_000);
         BigDecimal amount = new BigDecimal(random.nextInt(50_000, 200_000));
         int installment = random.nextInt(1, 120);
         double interestRate = random.nextDouble();
         Bank bank = BankFactory.getInstance().createRandom();
-        return new VehicleLoan(amount, installment, interestRate, bank);
+        return new VehicleLoan(id, amount, installment, interestRate, bank);
     }
 }
