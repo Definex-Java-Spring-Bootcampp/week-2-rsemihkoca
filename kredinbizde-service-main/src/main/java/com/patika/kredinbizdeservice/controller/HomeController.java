b83b4f2cdacd371818bfab28fb088328c0c8db73
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-24 22:17:57 +0300

    fully working solution:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/HomeController.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/HomeController.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.patika.kredinbizdeservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home(){
         return "Hello World!";
     }
