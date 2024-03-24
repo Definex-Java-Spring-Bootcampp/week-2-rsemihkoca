ff9ff05dc8d04c6cd4650d1c740481aa2e5e6b95
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-24 22:17:57 +0300

    fully working solution:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/CampaignWithCC.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/CampaignWithCC.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/CampaignWithCC.java
@@ -1,8 +1,15 @@
 package com.patika.kredinbizdeservice.model;
 
+import lombok.Getter;
 
-public class CampaignWithCCResponse {
+@Getter
+public class CampaignWithCC {
 
     private Campaign campaign;
     private CreditCard creditCard;
+
+    public CampaignWithCC(Campaign campaign, CreditCard creditCard) {
+        this.campaign = campaign;
+        this.creditCard = creditCard;
+    }
 }
