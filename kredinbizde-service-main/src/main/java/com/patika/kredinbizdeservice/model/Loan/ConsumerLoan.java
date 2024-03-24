1df5e61af5929d9b12493556f4c3c278d8df1bf9
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-24 20:07:09 +0300

    added week1 classes for init and swagger support:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/Loan/ConsumerLoan.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/Loan/ConsumerLoan.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/Loan/ConsumerLoan.java
@@ -1,8 +1,8 @@
 package com.patika.kredinbizdeservice.model.Loan;
 
-import com.patika.kredinbizdenservice.factory.BankFactory;
-import com.patika.kredinbizdenservice.model.Bank;
-import com.patika.kredinbizdenservice.enums.LoanType;
+import com.patika.kredinbizdeservice.factory.BankFactory;
+import com.patika.kredinbizdeservice.model.Bank;
+import com.patika.kredinbizdeservice.enums.LoanType;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -14,8 +14,8 @@ public class ConsumerLoan extends Loan {
     @Getter private LoanType loanType = LoanType.IHTIYAC_KREDISI;
     private List<Integer> installmentOptions;
 
-    private ConsumerLoan(BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
-        super(amount, installment, interestRate, bank);
+    private ConsumerLoan(Integer id, BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
+        super(id, amount, installment, interestRate, bank);
     }
 
 
@@ -24,17 +24,18 @@ public class ConsumerLoan extends Loan {
         //tc bul, maaşı bul
     }
 
-    public static ConsumerLoan create(BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
-        return new ConsumerLoan(amount, installment, interestRate, bank);
+    public static ConsumerLoan create(Integer id, BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
+        return new ConsumerLoan(id, amount, installment, interestRate, bank);
     }
 
     public static ConsumerLoan createRandom() {
         Random random = new Random();
+        int id = random.nextInt(1, 1_000_000);
         BigDecimal amount = new BigDecimal(random.nextInt(50_000, 200_000));
         int installment = random.nextInt(1, 120);
         double interestRate = random.nextDouble();
         Bank bank = BankFactory.getInstance().createRandom();
-        return new ConsumerLoan(amount, installment, interestRate, bank);
+        return new ConsumerLoan(id, amount, installment, interestRate, bank);
     }
 
 }
