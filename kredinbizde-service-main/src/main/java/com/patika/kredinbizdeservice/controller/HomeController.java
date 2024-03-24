68fe36add284fca18f4964a915d34709b679fcac
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-24 20:07:09 +0300

    added week1 classes for init and swagger support:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/HomeController.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/HomeController.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/HomeController.java
@@ -1,2 +1,13 @@
-package com.patika.kredinbizdeservice.controller;public class HomeController {
+package com.patika.kredinbizdeservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
 }
