b44709abe8f71a9e2c9b88d2751fe2874953c20a
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-24 20:07:09 +0300

    added week1 classes for init and swagger support:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/CreditCard.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/CreditCard.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/CreditCard.java
@@ -1,16 +1,12 @@
 package com.patika.kredinbizdeservice.model;
 
 
-import com.patika.kredinbizdenservice.factory.BankFactory;
+import com.patika.kredinbizdeservice.factory.BankFactory;
 import lombok.Getter;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
-public class CreditCard implements Product{
+public class CreditCard{
 
     private BigDecimal fee;
     @Getter private Bank bank;
