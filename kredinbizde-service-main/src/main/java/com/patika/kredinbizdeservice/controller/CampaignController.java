6c0c08ddb072f335219185664867389a33a717a5
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-24 22:17:57 +0300

    fully working solution:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/CampaignController.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/CampaignController.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/controller/CampaignController.java
@@ -1,4 +1,40 @@
 package com.patika.kredinbizdeservice.controller;
 
-public class BankController {
+import com.patika.kredinbizdeservice.model.Campaign;
+import com.patika.kredinbizdeservice.model.CampaignWithCC;
+import com.patika.kredinbizdeservice.service.ICampaignService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/campaigns")
+public class CampaignController {
+
+    private final ICampaignService campaignService;
+
+    public CampaignController(ICampaignService campaignService) {
+        this.campaignService = campaignService;
+    }
+
+    //   - C) Bankaların kredi kartlarını ve bu kartların kampanyalarını listeleyen end-point’i yazın.
+    //   - D) Sistemdeki bütün kampanyaları en güncelden eski tarihe doğru listeleyen end-point’i yazın.
+
+    @GetMapping("/WithCC")
+    public List<CampaignWithCC> getAllCampaigns() {
+        return campaignService.getAllCampaigns();
+    }
+
+    @GetMapping("/All")
+    public List<Campaign> getAllCampaignsOrdered() {
+        return campaignService.getAllCampaignsOrdered();
+    }
+
+
+
 }
+
+
